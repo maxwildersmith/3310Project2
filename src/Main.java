ffa86336443c812361b3ede37641f7f87c9d6af4
Maximum Wilder-Smith <maxwildersmith@gmail.com>
2019-10-30 23:56:26 -0800

    Finished knapsacks

--- src/Main.java
+++ src/Main.java
@@ -13,8 +13,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("\n###########################################\nCS3310 Project #2\nTannaz Damavandi\nby Maximum Wilder-Smith\n###########################################\n");
 
-        System.out.println(readItemsFromText());
-        //task1();
+        task1();
         task2();
 
         System.out.println("\n\nHave a Nice Day!");
@@ -30,8 +29,20 @@ public class Main {
                 double size = in.nextDouble();
                 if (size < 0)
                     throw new InputMismatchException();
-                boolean input = true;
-                ArrayList<Item> items = new ArrayList<>();
+
+                System.out.println("Read from file? (should be in the format of 'weight, profit \\n')(y/n): ");
+                boolean input = in.next().trim().toLowerCase().charAt(0) != 'y';
+                ArrayList<Item> items = null;
+                while(items==null&&!input){
+                    try {
+                        items = readItemsFromText();
+                    }catch (Exception e){
+                        System.out.println("File selection cancelled, switching to manual input");
+                        input=true;
+                    }
+                }
+                if(input)
+                    items= new ArrayList<>();
                 while (input) {
                     System.out.println("Enter the weight of the item: ");
                     double weight = in.nextDouble();
@@ -67,12 +78,19 @@ public class Main {
                 if (size < 0)
                     throw new InputMismatchException();
 
-                System.out.println("Read from file? (y/n): ");
+                System.out.println("Read from file? (should be in the format of 'weight, profit \\n')(y/n): ");
                 boolean input = in.next().trim().toLowerCase().charAt(0) != 'y';
                 ArrayList<Item> items = null;
-                while(items==null&&!input)
-                    items = readItemsFromText();
-                items = new ArrayList<>();
+                while(items==null&&!input){
+                    try {
+                        items = readItemsFromText();
+                    }catch (Exception e){
+                        System.out.println("File selection cancelled, switching to manual input");
+                        input=true;
+                    }
+                }
+                if(input)
+                    items= new ArrayList<>();
                 while (input) {
                     System.out.println("Enter the weight of the item: ");
                     double weight = in.nextDouble();
@@ -93,26 +111,34 @@ public class Main {
                 run = in.next().trim().toLowerCase().charAt(0) == 'y';
             } catch (InputMismatchException e) {
                 System.out.println("Please only enter positive numeric values");
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
             }
         }
     }
 
-    static ArrayList<Item> readItemsFromText() throws FileNotFoundException {
-        JFileChooser fileChooser = new JFileChooser("input.txt");
-        fileChooser.setFileFilter(new FileNameExtensionFilter("txt"));
+    static ArrayList<Item> readItemsFromText() throws Exception{
+        JFileChooser fileChooser = new JFileChooser(System.getProperty("java.class.path"));
+        fileChooser.setFileFilter(new FileNameExtensionFilter("txt files","txt"));
         int returnVal = fileChooser.showOpenDialog(null);
-        Scanner file;
-        if(returnVal == JFileChooser.APPROVE_OPTION)
-            file = new Scanner(fileChooser.getSelectedFile());
+        try {
+            Scanner file;
+        if(returnVal == JFileChooser.APPROVE_OPTION) {
+                file = new Scanner(fileChooser.getSelectedFile());
+        }
         else
-            return null;
+            throw new Exception();
         ArrayList<Item> items = new ArrayList<>();
         while(file.hasNextLine()){
             String[] item = file.next().split(",");
-            items.add(new Item(Double.parseDouble(item[0]),Double.parseDouble(item[1])));
+            if(item.length>1)
+                items.add(new Item(Double.parseDouble(item[0]),Double.parseDouble(item[1])));
         }
         return items;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (NumberFormatException e) {
+            System.out.println("Make sure the file is formatted properly in the form: \nweight,profit\nweight,profit\n...");
+            return null;
+        }
+        return null;
     }
 }
